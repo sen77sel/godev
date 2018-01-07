4cdb74ffbdd62c288a0bd8711e62ce8b0e33e4b0
sen77sel <sselvaraj@gmail.com>
2018-01-06 22:57:44 -0500

    add pkgs

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sen77sel/godev/stringutil"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Printf(stringutil.Reverse("\n!oG ,olleH"))
 }
